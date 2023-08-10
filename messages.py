fa1d00b0ca81fdd9c527893dc714b2130c54bf30
JorgeKz <jorgekzbra@gmail.com>
2023-08-10 15:10:28 -0300

    Climate-data-to-swat

---
+++ messages.py
@@ -0,0 +1,22 @@
+class Messages:
+    CLOSE_BUTTON = "Sair"
+    CLOSE_MESSAGE = ("Processamento finalizado. Os arquivos foram salvos na "
+                     "pasta 'output_climate2swat'.")
+    EXECUTE_BUTTON = "Processar Dados"
+    INPUT_ERROR_MESSAGE = ("Selecione a pasta de entrada antes de executar "
+                           "o processamento.")
+    INPUT_INSTRUCTIONS = ("Esta aplicação processa arquivos CSV, contendo "
+                          "dados do INMET, para o formato \n"
+                          "de entrada usado no modelo SWAT.\n\n"
+                          "Passos para executar:\n"
+                          "1. Clique no botão abaixo para selecionar a pasta "
+                          "contendo os arquivos CSV.\n"
+                          "2. Será exibido o número de arquivos a serem "
+                          "processados.\n"
+                          "3. Clique no botão 'Processar Dados' para iniciar "
+                          "o processamento.\n"
+                          "4. Após a conclusão, os arquivos processados serão "
+                          "salvos na pasta de saída.\n"
+                          )
+    NO_CSV_FILES_MESSAGE = "Nenhum arquivo CSV foi encontrado na pasta."
+    SELECT_FOLDER_BUTTON = "Selecionar Pasta"
